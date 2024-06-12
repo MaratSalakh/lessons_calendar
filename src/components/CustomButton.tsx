034f4028892c491f4542b576823a76d5ca252ab5
Marat Salakh <marat.salakh.dev@gmail.com>
2024-06-12 19:47:16 +0200

    change button colors

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -5,7 +5,7 @@ export const CustomButton = (props: { children: string }) => {
     // <button className="mt-4 h-full rounded-3xl bg-violet-400 p-2 text-white hover:bg-violet-500">
     //   {children}
     // </button>
-    <button className="bg-brand hover:bg-darkBrand btn w-full rounded-3xl text-white">
+    <button className="btn w-full rounded-3xl bg-primary text-white hover:bg-secondary">
       {children}
     </button>
   );
