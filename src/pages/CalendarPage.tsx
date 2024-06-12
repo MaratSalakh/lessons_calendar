9b9214c86f902dc5a231c3d2d6efc1c5d849d6e8
Marat Salakh <marat.salakh.dev@gmail.com>
2024-06-12 20:53:37 +0200

    add calendar page

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,5 +1,17 @@
 import { CustomMenu } from "../components/CustomMenu";
+import { CustomNavBar } from "../components/CustomNavBar";
 
 export const CalendarPage = () => {
-  return <CustomMenu />;
+  return (
+    <div className="m-5 flex">
+      <div className="w-2/12">
+        <div className="mt-5">
+          <CustomMenu />
+        </div>
+      </div>
+      <div className="w-10/12">
+        <CustomNavBar />
+      </div>
+    </div>
+  );
 };
