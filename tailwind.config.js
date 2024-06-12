eaa4b75d8ddf8ffc6e78c1876fe80812f78db1c4
Marat Salakh <marat.salakh.dev@gmail.com>
2024-06-12 11:03:35 +0200

    add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      brand: "#8D7FC7",
+      darkBrand: "#62588b",
+    },
     extend: {},
   },
   plugins: [daisyui],
