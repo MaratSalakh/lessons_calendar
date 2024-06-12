ac4a546e7eb3cd11e059c6f4d628ae39df99fe62
Marat Salakh <marat.salakh.dev@gmail.com>
2024-06-12 19:47:29 +0200

    create custom menu

---
+++ src/components/CustomMenu.tsx
@@ -0,0 +1,93 @@
+import {
+  Home,
+  CalendarDays,
+  Wallet,
+  Headset,
+  Trophy,
+  Puzzle,
+  FolderOpen,
+  Settings,
+  CircleHelp,
+} from "lucide-react";
+
+import cn from "classnames";
+
+export const CustomMenu = () => {
+  const linkClasses = cn("rounded-none", "rounded-e-3xl", "hover:bg-primary");
+
+  return (
+    <div className="m-3 flex w-2/12 flex-col gap-3 rounded-3xl bg-neutral pb-5 pr-5 pt-5">
+      <img
+        className="self-center"
+        src="logoWithTitle.svg"
+        alt="logo with title"
+      />
+      <ul className="menu w-56 pl-0">
+        <li>
+          <a className={linkClasses}>
+            <Home />
+            Главная
+          </a>
+        </li>
+        <li>
+          <a className={linkClasses}>
+            <CalendarDays />
+            Расписание
+          </a>
+        </li>
+        <li>
+          <a className={linkClasses}>
+            <Wallet />
+            Оплата
+          </a>
+        </li>
+        <li>
+          <a className={linkClasses}>
+            <Trophy />
+            Достижения
+          </a>
+        </li>
+        <li>
+          <a className={linkClasses}>
+            <Puzzle />
+            Тренажеры
+          </a>
+        </li>
+        <li>
+          <a className={linkClasses}>
+            <FolderOpen />
+            Библиотека
+          </a>
+        </li>
+        <li>
+          <a className={linkClasses}>
+            <Headset />
+            Проверка связи
+          </a>
+        </li>
+        <li>
+          <a className={linkClasses}>
+            <Settings />
+            Настройки
+          </a>
+        </li>
+        <li>
+          <a className={linkClasses}>
+            <CircleHelp />
+            Вопросы
+          </a>
+        </li>
+      </ul>
+      <div className="bg-gift ml-3 mr-3 flex flex-col items-start gap-3 rounded-2xl bg-white bg-right-bottom bg-no-repeat p-3">
+        <h2 className="text-lg">Учитесь бесплатно</h2>
+        <p className="text-xs font-light">
+          Приводите друзей с детьми заниматься в Sirius Future и получайте
+          подарки!
+        </p>
+        <button className="btn rounded-3xl bg-[#D8ECFF] hover:bg-white">
+          <span className="font-light">Узнать</span>
+        </button>
+      </div>
+    </div>
+  );
+};
